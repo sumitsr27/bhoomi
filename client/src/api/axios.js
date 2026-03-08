bf7f500018ef31f23741108ecaf3dc19c626c59f
sumit sr <sumitsr5603@gmail.com>
2026-03-08 12:53:12 +0530

    fix backend url for deployment

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,22 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: '/api',
-  headers: { 'Content-Type': 'application/json' },
+  baseURL: import.meta.env.VITE_BACKEND_URL + "/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
-
-api.interceptors.response.use(
-  (res) => res,
-  (err) => {
-    if (err.response?.status === 401) {
-      localStorage.removeItem('token');
-      delete api.defaults.headers.common['Authorization'];
-      if (window.location.pathname !== '/login' && window.location.pathname !== '/register') {
-        window.location.href = '/login';
-      }
-    }
-    return Promise.reject(err);
-  }
-);
 
 export default api;
